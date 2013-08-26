ba5d6dd1e34f9c323b182a3a30efad4f34d753be
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-08-26 23:16:32 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sql2typo/trunk@79129 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -4,11 +4,23 @@ if (!defined ('TYPO3_MODE'))
   die ('Access denied.');
 }
 
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  // 
+  // INDEX
+  
+  // Enables the Include Static Templates
+  // Flexform Configuration
+  // Add pagetree icons
+
+
+
   ///////////////////////////////////////
   // 
   // Enables the Include Static Template
 
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/', 'SQL to TYPO3');
+t3lib_extMgm::addStaticFile( $_EXTKEY, 'static/', 'SQL to TYPO3' );
   // Enables the Include Static Template
 
 
@@ -16,15 +28,23 @@ t3lib_extMgm::addStaticFile($_EXTKEY,'static/', 'SQL to TYPO3');
   //
   // Flexform Configuration
   
-  t3lib_div::loadTCA('tt_content');
-  $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages,recursive';
-  // Remove the default tt_content fields layout, select_key, pages and recursive.
-//  $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
-  // Display the field pi_flexform
-//  t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/pi1/flexform.xml');
-  // Register our file with the flexform structure
-  t3lib_extMgm::addPlugin(array('LLL:EXT:sql2typo/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1', 'EXT:sql2typo/ext_icon.gif'),'list_type');
-  // Add the Flexform to the Plugin List
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages,recursive';
+t3lib_extMgm::addPlugin( array( 'LLL:EXT:sql2typo/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1', 'EXT:sql2typo/ext_icon.gif' ), 'list_type' );
   // Flexform Configuration
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  // 
+  // Add pagetree icons
+
+$TCA['pages']['columns']['module']['config']['items'][ ] = 
+  array('AWO Mittelrhein', 'sql2typo', t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif' );
+t3lib_SpriteManager::addTcaTypeIcon('pages', 'contains-sql2typo',
+  t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif' );
+  // Add pagetree icons
+
+
 
 ?>
